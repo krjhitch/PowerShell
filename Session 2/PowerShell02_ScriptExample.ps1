a5fe5f825def4e55ae02a0a184b638cb9a77e769
krjhitch <krjhitch@gmail.com>
2017-07-12 11:43:33 -0400

    initial commit

---
+++ Session 2/PowerShell02_ScriptExample.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()]
+param(
+    [ValidateScript({$_ -gt 0})]
+    [Parameter(Mandatory=$true)]
+    [Int]$Number1,
+    [ValidateScript({$_ -lt 0})]
+    [Parameter(Mandatory=$true)]
+    [Int]$Number2
+)
+$answer = $Number1 * $Number2
+Write-Verbose "Multiplying $Number1 by $Number2, result = $answer"
+
+return $answer
