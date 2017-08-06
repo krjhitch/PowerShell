ea28ad524bde76f6d30263069223e0fb2ad08cee
krjhitch <krjhitch@gmail.com>
2017-08-06 00:09:35 -0400

    added sections

--- Session 3/PowerShell.ps1
+++ Session 3/PowerShell.ps1
@@ -1,1 +1,24 @@
 Test
+
+# Remote Shutdown
+ ## Command
+ ## PowerShell
+ ## Modify Firewall
+
+# PowerShell Remoting
+ ## Building Credentials
+ ## Kerberos Double-Hop
+ ## Moving Files
+ ## Interactive
+ ## Non-Interactive
+ ## Sequential vs Threaded
+ ## PS-Session / Get-History
+
+#Jobs
+ #Adding/removing jobs
+ #Start-Sleep
+
+
+
+
+
